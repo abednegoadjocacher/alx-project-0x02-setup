c03a69cb3d8f91d9df013884df240db8f195a5ab
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-09-24 23:35:02 +0000

    Project directory created

---
+++ alx-project-0x02/alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+const Header: React.FC = () => (
+    <header className="w-full px-8 py-4 bg-gray-900 text-white flex items-center justify-between">
+        <h1 className="m-0 text-xl font-bold">My Project</h1>
+        <nav>
+            <Link href="/" className="text-white no-underline mr-4 hover:underline">Mainhome</Link>
+            <Link href="/home" className="text-white no-underline hover:underline">Home</Link>
+            <Link href="/about" className="text-white no-underline hover:underline">About</Link>
+        </nav>
+    </header>
+);
+
+export default Header;
